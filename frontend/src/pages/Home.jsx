dc3fe3a438410f7be453773d2f2c86c67b0d7efc
amantechDevise <amantechdevise@gmail.com>
2025-08-11 18:20:52 +0530

    reslove problam

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -664,7 +664,7 @@ const Home = () => {
                     </div>
                   </div>
                   <div className="flex-[1_1_0%] flex flex-col justify-between items-stretch">
-                    <div className="lights flex justify-end items-start gap-[3.1781rem] pe-[4.4781rem] max-lg:hidden -m-8">
+                    <div className="lights flex justify-end items-start gap-[3.1781rem] pe-[4.4781rem] max-lg:hidden -m-[18px]">
                       <div className="h-[170px] overflow-hidden">
                         <img
                           src={lightOne}
