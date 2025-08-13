551829de86ca3fc81421b107a0574f37bb9e7a16
amantechDevise <amantechdevise@gmail.com>
2025-08-13 17:09:20 +0530

    final

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -850,7 +850,7 @@ const Home = () => {
                       speed={12}
                     />
                     <h4 className="rounded-[1.625rem] 2xl:text-[1.5rem] sm:text-[1.3rem] text-[1.2rem]  leading-[2.125rem] bg-white/60 w-full lg:h-[7.3125rem] h-[5.315rem] flex justify-center items-center text-center px-5">
-                      Digital Solutions Delivered
+                     Digital Marketing Solutions Delivered
                     </h4>
                   </div>
                 </div>
